e2e62bdd39e729a2fbb073039bef7780c7d79011
PepijnBullens <pepijnpersoonlijk@gmail.com>
2025-07-09 22:52:21 +0200

    Added puzzle 06

---
+++ puzzle06/part03/index.js
@@ -0,0 +1,60 @@
+const fs = require("node:fs");
+
+function readFile(path, callback) {
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      callback(err, null);
+      return;
+    }
+    callback(null, data);
+  });
+}
+
+readFile("../input.txt", (err, data) => {
+  if (err) return;
+  const lines = data.split(/\r?\n/).filter((line) => line.trim() !== "");
+
+  const skySize = 1000;
+  const frameSize = 500;
+  const pictureInterval = 31556926;
+  const totalPictures = 1000;
+
+  const birds = lines.map((line) => {
+    const [x, y] = line.split(",").map(Number);
+    return { speedX: x, speedY: y };
+  });
+
+  let totalBirdsInAllPictures = 0;
+
+  for (let picture = 0; picture < totalPictures; picture++) {
+    const seconds = picture * pictureInterval;
+    let birdsInFrame = 0;
+
+    for (let i = 0; i < birds.length; i++) {
+      const bird = birds[i];
+
+      let x = (bird.speedX * seconds) % skySize;
+      let y = (bird.speedY * seconds) % skySize;
+
+      if (x < 0) x += skySize;
+      if (y < 0) y += skySize;
+
+      const frameStart = (skySize - frameSize) / 2;
+      const frameEnd = frameStart + frameSize - 1;
+
+      if (
+        x >= frameStart &&
+        x <= frameEnd &&
+        y >= frameStart &&
+        y <= frameEnd
+      ) {
+        birdsInFrame++;
+      }
+    }
+
+    totalBirdsInAllPictures += birdsInFrame;
+  }
+
+  console.log(totalBirdsInAllPictures);
+});
